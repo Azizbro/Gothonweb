d0a74edeafcba7d06a61c753fb2dab22a648242e
Zarif Aziz <zarif_bionicle@hotmail.com>
2018-03-18 17:31:59 +1100

    Managed to upload images. Only works with JPEG, not PNG

--- bin/app.py
+++ bin/app.py
@@ -1,7 +1,8 @@
 import web
 
 urls = (
-    '/hello', 'Index'
+    '/hello', 'Index',
+    '/upload', 'Upload',
 )
 
 app = web.application(urls, globals())
@@ -18,18 +19,9 @@ class Index(object):
         # instead of just a string we're now using web.input to get data
         # from the browser. This func parses the ?name=Frank part of the
         # URL you give it, and then returns an object for you to use
-        form = web.input(name="Nobody", greet=None, myfile={})
-
-        # Uploading a file
-        # Where you want to store the file in
-        filedir = 'C:\Users\Zarif\OneDrive\Documents\Python Projects\gothonweb'
-        # To check if the file-object is created
-        if 'myfile' in form:
-            filepath= form.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
-            filename= filepath.split('/')[-1] # splits the / and chooses the last part (the filename with extension)
-            fout = open(filedir +'/'+ filename,'w') # creates the file where the uploaded file should be stored
-            fout.write(form.myfile.file.read()) # writes the uploaded file to the newly created file.
-            fout.close() # closes the file, upload complete.
+        # This function takes a key=value set of defaults
+        # So make sure each variable has a default value
+        form = web.input(name="Nobody", greet=None)
 
         greeting = "%s, %s" % (form.greet, form.name)
         # Shows that the name of the function you call on render is just
@@ -37,6 +29,31 @@ class Index(object):
         # everything else can refer to index
         return render.index_laid_out(greeting = greeting)
 
+class Upload(object):
+    def GET(self):
+        return render.upload_form()
+
+    def POST(self):
+        # This function takes a key=value set of defaults
+        # So make sure each variable has a default value
+        x = web.input(myfile = {})
+
+        # Uploading a file
+        # Where you want to store the file in
+        filedir = 'C:\Users\Zarif\OneDrive\Documents\Python Projects\gothonweb'
+        # To check if the file-object is created
+        if 'myfile' in x:
+            filepath= x.myfile.filename.replace('\\','/') # replaces the windows-style slashes with linux ones.
+            filename= filepath.split('/')[-1] # splits the / and chooses the last part (the filename with extension)
+            #'wb' means binary mode
+            fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
+            fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
+            fout.close() # closes the file, upload complete.
+
+        # Go to another web page where you have the output message
+        return render.upload()
+
+# What does this mean?
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
